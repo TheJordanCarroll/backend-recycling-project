ffdd449e78ac28cf7a99ce05880f63cea7ccdd3b
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2021-03-05 21:24:40 -0500

    API

--- config/routes.rb
+++ config/routes.rb
@@ -1,59 +1,97 @@
 Rails.application.routes.draw do
-  get 'user_item_categories/index'
-  get 'user_item_categories/new'
-  get 'user_item_categories/create'
-  get 'user_item_categories/show'
-  get 'user_item_categories/edit'
-  get 'user_item_categories/update'
-  get 'user_item_categories/destroy'
-  get 'user_items/index'
-  get 'user_items/new'
-  get 'user_items/create'
-  get 'user_items/show'
-  get 'user_items/edit'
-  get 'user_items/update'
-  get 'user_items/destroy'
-  get 'fav_sites/index'
-  get 'fav_sites/new'
-  get 'fav_sites/create'
-  get 'fav_sites/show'
-  get 'fav_sites/edit'
-  get 'fav_sites/update'
-  get 'fav_sites/destroy'
-  get 'categories/index'
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/show'
-  get 'categories/edit'
-  get 'categories/update'
-  get 'categories/destroy'
-  get 'item_sites/index'
-  get 'item_sites/new'
-  get 'item_sites/create'
-  get 'item_sites/show'
-  get 'item_sites/edit'
-  get 'item_sites/update'
-  get 'item_sites/destroy'
-  get 'sites/index'
-  get 'sites/new'
-  get 'sites/create'
-  get 'sites/show'
-  get 'sites/edit'
-  get 'sites/update'
-  get 'sites/destroy'
-  get 'comments/index'
-  get 'comments/new'
-  get 'comments/create'
-  get 'comments/show'
-  get 'comments/edit'
-  get 'comments/update'
-  get 'comments/destroy'
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
+  # get 'user_item_categories/index'
+  # get 'user_item_categories/new'
+  # get 'user_item_categories/create'
+  # get 'user_item_categories/show'
+  # get 'user_item_categories/edit'
+  # get 'user_item_categories/update'
+  # get 'user_item_categories/destroy'
+
+
+
+
+  # get 'user_items/index'
+  # get 'user_items/new'
+  # get 'user_items/create'
+  # get 'user_items/show'
+  # get 'user_items/edit'
+  # get 'user_items/update'
+  # get 'user_items/destroy'
+
+
+
+
+  # get 'fav_sites/index'
+  # get 'fav_sites/new'
+  # get 'fav_sites/create'
+  # get 'fav_sites/show'
+  # get 'fav_sites/edit'
+  # get 'fav_sites/update'
+  # get 'fav_sites/destroy'
+
+
+
+
+
+  # get 'categories/index'
+  # get 'categories/new'
+  # get 'categories/create'
+  # get 'categories/show'
+  # get 'categories/edit'
+  # get 'categories/update'
+  # get 'categories/destroy'
+
+
+
+
+  # get 'item_sites/index'
+  # get 'item_sites/new'
+  # get 'item_sites/create'
+  # get 'item_sites/show'
+  # get 'item_sites/edit'
+  # get 'item_sites/update'
+  # get 'item_sites/destroy'
+
+
+
+
+  # get 'sites/index'
+  # get 'sites/new'
+  # get 'sites/create'
+  # get 'sites/show'
+  # get 'sites/edit'
+  # get 'sites/update'
+  # get 'sites/destroy'
+  ######### SITES ###############
+  
+  get '/sites', to: 'sites#index'
+  # get '/sites/new', to: 'sites#new', as: 'new_site'
+  # post '/sites', to: 'sites#create'
+  # get '/sites/:id', to: 'sites#show', as: 'tag'
+  # get '/sites/:id/edit', to: 'sites#edit', as: 'edit_site'
+  # delete '/sites/:id', to: 'sites#destroy'
+  # patch '/sites/:id', to: 'sites#update'
+  
+  
+  
+  # get 'comments/index'
+  # get 'comments/new'
+  # get 'comments/create'
+  # get 'comments/show'
+  # get 'comments/edit'
+  # get 'comments/update'
+  # get 'comments/destroy'
+  
+  
+  
+  
+  get '/users', to: 'users#index'
+  # get 'users/index'
+  # get 'users/new'
+  # get 'users/create'
+  # get 'users/show'
+  # get 'users/edit'
+  # get 'users/update'
+  # get 'users/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,35 +10,73 @@ response = RestClient.get 'https://data.cityofnewyork.us/resource/sxx4-xhzg.json
 array_of_bins = JSON.parse(response)
 
 array_of_bins.each do |bin|
-    puts bin["borough"] 
+    # puts bin["longi tude"] 
+    Site.create(address: bin["address"], borough: bin["borough"], name: bin["park_site_name"], website: "https://www1.nyc.gov/assets/dsny/site/home", latitude: bin["latitude"], longitude: bin["longitude"], rating: 0, accepted_items: "paper,cardboard, plastic, glass, cartons", image: "https://upload.wikimedia.org/wikipedia/en/0/08/DSNY_logo_2016.jpg", hours: "7am-3pm", twitter: "https://twitter.com/nycsanitation?lang=en", ig: "https://www.instagram.com/nycsanitation/?hl=en", facebook: "https://www.facebook.com/nycsanitation/")
 end
-# puts bin["borough"] 
+
+
+response = RestClient.get 'https://data.cityofnewyork.us/resource/qnjm-wvu5.json'
+array_of_textiles = JSON.parse(response)
+
+array_of_textiles.each do |textile|
+    # puts textile["address"]
+    Site.create(address: textile["address"], borough: textile["borough"], name: textile["vendor_name"], website: textile["website"], latitude: textile["latitude"], longitude: textile["longitude"], rating: 0, accepted_items: textile["items_accepted"], image: "https://static.dezeen.com/uploads/2016/08/new-york-2020-skyline_dezeen_ss-852x609.jpg", hours: "9am-5pm", twitter: "https://twitter.com/wearablecollect?lang=en", ig: "https://www.instagram.com/wearablecollections/?hl=en", facebook: " https://www.facebook.com/wearablecollections/services/?ref=page_internal")
+end
+
+
+
+
+response = RestClient.get 'https://data.cityofnewyork.us/resource/wshr-5vic.json'
+array_of_e_waste = JSON.parse(response)
+
+array_of_e_waste.each do |waste|
+    # puts textile["address"]
+    Site.create(address: waste["address"], borough: waste["borocd"], name: waste["dropoff_sitename"], website: "https://www1.nyc.gov/assets/dsny/site/contact/e-waste-pickup-request", latitude: waste["latitude"], longitude: waste["longitude"], rating: 0, accepted_items: "electronics", image: "https://upload.wikimedia.org/wikipedia/commons/c/cf/Radio_City_Music_Hall_Panorama.jpg", hours: "9am-5pm", twitter: "https://twitter.com/nysdec?lang=en", ig: "https://www.instagram.com/nysdec/?hl=en", facebook: " https://www.facebook.com/NYSDEC/")
+end
+
+
+
 # puts bin["address"] 
+# puts bin["borough"] 
 # puts bin["park_site_name"]
+# website: "https://www1.nyc.gov/assets/dsny/site/home"
 # puts bin["latitude"]
+# puts bin["longitude"] 
+# rating:"0"
+# accepted_items: "paper,cardboard, plastic, glass, cartons"
+# image: "https://upload.wikimedia.org/wikipedia/en/0/08/DSNY_logo_2016.jpg" 
+# hours: "7am-3pm"
+# twitter: "https://twitter.com/nycsanitation?lang=en"
+# ig: "https://www.instagram.com/nycsanitation/?hl=en"
+# facebook: "https://www.facebook.com/nycsanitation/"
 
-# User.create(name: "Oscar", username: "oscar123", password: "abc123", email: "oscarthegrouch@gmail.com", address: "123 Sesame Street", borough: "Manhattan", image: "https://thinkchristian-webassets.imgix.net/articleImages/2019/sesame-street-at-50-craft.jpg?crop=focalpoint&fit=crop&fp-x=0.5&fp-y=0.5&h=500&ixlib=php-1.1.0&q=80&w=800&s=fccd8dc1bbdd22d711ef73af868a65e5")
-# User.create(name: "Captain Planet", username: "captain123", password: "abc123", email: "captainplanet@gmail.com", address: "123 Planet Street", borough: "Brooklyn", image: "https://pbs.twimg.com/profile_images/939549969958051840/zs3ndSvV_400x400.jpg")
 
-# Category.create(name: "Paper", video: "https://www.youtube.com/watch?v=RR_218EtLJU", description: "newspapers, magazines, catalogs, phone books, mixed paper white and colored paper (lined, copier, computer; staples are ok) mail and envelopes (any color; window envelopes are ok) receipts paper bags (handles ok) wrapping paper soft-cover books (phone books, paperbacks, comics, etc.; no spiral bindings) (schools should follow their school book recycling procedures)")
-# Category.create(name: "Cardboard", video: "https://www.youtube.com/watch?v=Rlvd-dIAHCE", description: "cardboard egg cartons cardboard trays smooth cardboard (food and shoe boxes, tubes, file folders, cardboard from product packaging) pizza boxes (remove and discard soiled liner; recycle little plastic supporter with rigid plastics) paper cups (waxy lining ok if cups are empty and clean; recycle plastic lids with rigid plastics) corrugated cardboard boxes (flattened and tied together with sturdy twine)")
 
-# Item.create(user_id: 1, name: "Pizza Box")
-# Item.create(user_id: 2, name: "Wine Bottle")
+
+
+
+User.create(name: "Oscar", username: "oscar123", password: "abc123", email: "oscarthegrouch@gmail.com", address: "123 Sesame Street", borough: "Manhattan", image: "https://thinkchristian-webassets.imgix.net/articleImages/2019/sesame-street-at-50-craft.jpg?crop=focalpoint&fit=crop&fp-x=0.5&fp-y=0.5&h=500&ixlib=php-1.1.0&q=80&w=800&s=fccd8dc1bbdd22d711ef73af868a65e5")
+User.create(name: "Captain Planet", username: "captain123", password: "abc123", email: "captainplanet@gmail.com", address: "123 Planet Street", borough: "Brooklyn", image: "https://pbs.twimg.com/profile_images/939549969958051840/zs3ndSvV_400x400.jpg")
+
+Category.create(name: "Paper", video: "https://www.youtube.com/watch?v=RR_218EtLJU", description: "newspapers, magazines, catalogs, phone books, mixed paper white and colored paper (lined, copier, computer; staples are ok) mail and envelopes (any color; window envelopes are ok) receipts paper bags (handles ok) wrapping paper soft-cover books (phone books, paperbacks, comics, etc.; no spiral bindings) (schools should follow their school book recycling procedures)")
+Category.create(name: "Cardboard", video: "https://www.youtube.com/watch?v=Rlvd-dIAHCE", description: "cardboard egg cartons cardboard trays smooth cardboard (food and shoe boxes, tubes, file folders, cardboard from product packaging) pizza boxes (remove and discard soiled liner; recycle little plastic supporter with rigid plastics) paper cups (waxy lining ok if cups are empty and clean; recycle plastic lids with rigid plastics) corrugated cardboard boxes (flattened and tied together with sturdy twine)")
+
+Item.create(user_id: 1, name: "Pizza Box")
+Item.create(user_id: 2, name: "Wine Bottle")
 
 # Site.create(address: "2040 White Plains Rd", borough: "Bronx", 
 #  "Staples", website: "www.staples.com", latitude: "40.85171272", longitude: "-73.86778588", rating: 5, accepted_items: "E-Waste", image: "https://assets1.chainstoreage.com/styles/content_sm/s3/teaser_image_481402.jpg?itok=d79TLBm-", hours: "9-5", twitter: "https://twitter.com/staples?lang=en", ig: "https://www.instagram.com/staples/?hl=en", facebook: "https://www.facebook.com/staples/")
 # Site.create(address: "940 Madison Ave", borough: "Manhattan", name: "Apple Store", website: "https://www.apple.com/", latitude: "40.77326199", longitude: "-73.96436877", rating: 4, accepted_items: "E-Waste", image: "https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?202103011306", hours: "9-5", twitter: "https://twitter.com/apple?lang=en", ig: "https://www.instagram.com/apple/?hl=en", facebook: "https://www.facebook.com/apple/")
 
-# Comment.create(user_id: 1, site_id: 1, content: "Very clean location.")
-# Comment.create(user_id: 2, site_id: 2, content: "Efficient recycling process.")
+Comment.create(user_id: 1, site_id: 1, content: "Very clean location.")
+Comment.create(user_id: 2, site_id: 2, content: "Efficient recycling process.")
 
-# CategorySite.create(category_id: 1, site_id: 1)
-# CategorySite.create(category_id: 2, site_id: 2)
+CategorySite.create(category_id: 1, site_id: 1)
+CategorySite.create(category_id: 2, site_id: 2)
 
-# ItemCategory.create(category_id: 1, item_id: 1)
-# ItemCategory.create(category_id: 2, item_id: 2)
+ItemCategory.create(category_id: 1, item_id: 1)
+ItemCategory.create(category_id: 2, item_id: 2)
 
-# UserSite.create(user_id: 1, site_id: 1)
-# UserSite.create(user_id: 2, site_id: 2)
+UserSite.create(user_id: 1, site_id: 1)
+UserSite.create(user_id: 2, site_id: 2)
 
